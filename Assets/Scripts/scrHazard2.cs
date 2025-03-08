802e313f39168108cb33e99a954a7da828090714
Trey547 <traveonpace63@gmail.com>
2025-03-08 05:30:53 -0800

    Hazard Script Update
    I updated a script that will be used to instantiate the falling objects in our game.

--- Assets/Scripts/scrHazard2.cs
+++ Assets/Scripts/scrHazard2.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class scrHazard2 : MonoBehaviour
 {
+    public float frequency = 1.0f;
+    public GameObject box;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +17,28 @@ public class scrHazard2 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Spawn()
+    {
+        WaitForSeconds delay = new WaitForSeconds(frequency);
+
+        while (true)
+        {
+            yield return delay;
+
+            Instantiate(box, transform.position, Quaternion.identity);
+            Destroy(Instantiate(box, transform.position, Quaternion.identity), 2f);
+        }
     }
 }
